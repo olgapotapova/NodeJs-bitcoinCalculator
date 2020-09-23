1bffb7b4e271bf3ba4ba61aaa654a15581d644f9
Opilane TTHK <opilane@tthk.ee>
2020-09-23 10:38:39 +0300

    commit

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const bodyParser= require('bodyParser');
+const axios = require('axios');
+const app = express();
+
+app.use(bodyParser.urlencoder({extended:true}));
+
+app.get('/',(req, res)=>{
+    res.sendFile(__dirname+'/index.html');
+});
+
+app.post('/',(req, res)=>{
+    let url = 'https://api.coindesk.com/v1/bpi/currentprice/eur.json';
+    let currency = req.body.currency;
+    console.log (currency);
+
+    axios.get(url)
+    .then(function(response){
+        let rate;
+        let code;
+        if(currency==='EUR'){
+            rate = response.data.bpi.EUR.rate;
+            code = response.data.bpi.EUR.code;
+        }else{
+            rate = response.data.bpi.USD.rate;
+            code = response.data.bpi.USD.code;
+        }
+        let disclainer  = response.data.disclaimer;
+        res.write('<p>${rate} ${code}<p>');
+        res.write('<p>${disclaimer}</p>');
+        res.send();
+    })
+    .catch(function(error){
+        console.log(error);
+    });
+})
+app.listen(3000, ()=>{
+    console.log('server is running on localhost:3000')
+})
